23ecb64841a12228a6f41f832372169d4666aa10
polyskull <drycerealminecraft@gmail.com>
2023-07-28 08:56:56 -0400

    Update to 0.2
    GloomwoodInspector v0.2
    + Added toggle menu keybind
    - Removed "Target Position" stats
    + Added Velocity, MoveDirection, Idle, Jumping, Leaning, Limping, Crouching, Eavesdropping, Grounded, NearGrounded, Sneaking, Mantling, HoldingMantle, OnClimbable, IsRope, Sliding, OnMovingPlatform, FallHeight and LastFallHeight stats.
    + Added new label explaining how to toggle menu
    + Added Custom "COORDTP" command to allow teleporting to specific coordinates.

--- GloomwoodInspector/InspectorMod.cs
+++ GloomwoodInspector/InspectorMod.cs
@@ -18,13 +18,18 @@ using GloomwoodInspector.Commands;
 using static HarmonyLib.AccessTools;
 using System.Collections.Generic;
 
-[assembly: MelonInfo(typeof(InspectorMod), "GloomwoodInspector", "0.1", "polyskull")]
+[assembly: MelonInfo(typeof(InspectorMod), "GloomwoodInspector", "0.2", "polyskull")]
 namespace GloomwoodInspector
 {
     public class InspectorMod : MelonMod
     {
         private static bool quitting = false;
+        private static bool drawMenu = true;
+
+        // Keybinds
+        private static KeyCode toggleMenu = KeyCode.Keypad1;
         private static KeyCode tpToCursor = KeyCode.Keypad7;
+
         private static LayerMask cursorLayer = LayerHelper.CreateMask(new LayerTypes[]
         {
             LayerTypes.AI,
@@ -67,6 +72,11 @@ namespace GloomwoodInspector
                         GameManager.Player.position = ReusableArrayHolder.RaycastHits[0].point;
                     }
                 }
+
+                if(UnityEngine.Input.GetKeyDown(toggleMenu))
+                {
+                    drawMenu = !drawMenu;
+                }
             }
         }
 
@@ -74,32 +84,45 @@ namespace GloomwoodInspector
         {
             if(!quitting)
             {
-                GUI.Label(new Rect(0, 0, 500, 50), "GloomwoodInspector " + this.Info.Version);
-                PlayerEntity? player = GameManager.Player;
-                if (player != null)
+                if (drawMenu)
                 {
-                    // Every property to draw
-                    string[] stats =
+                    GUI.Label(new Rect(0, 0, 500, 50), "GloomwoodInspector " + this.Info.Version);
+                    GUI.Label(new Rect(0, 15, 500, 50), "Press Numpad1 to toggle menu");
+                    PlayerEntity? player = GameManager.Player;
+                    if (player != null)
                     {
-                        "x: " + player.Position.x,
-                        "y: " + player.Position.y,
-                        "z: " + player.Position.z,
-                        "Target x: " + player.TargetPoint.x,
-                        "Target y: " + player.TargetPoint.y,
-                        "Target z: " + player.TargetPoint.z
-                    };
+                        PlayerMovement mobility = player.Mobility;
+                        Vector3 velocity = mobility.MoveVelocity;
+                        Vector3 dir = mobility.MoveDirection;
 
-                    int y = 0;
-                    foreach (string s in stats)
-                    {
-                        y += 15;
-                        GUI.Label(new Rect(0, y, 500, 50), s);
+                        // Every property to draw
+                        string[] stats =
+                        {
+                            "x: " + player.Position.x,
+                            "y: " + player.Position.y,
+                            "z: " + player.Position.z,
+                            string.Format("Velocity: {0}, {1}, {2}", velocity.x, velocity.y, velocity.z),
+                            string.Format("MoveDirection: {0}, {1}, {2}", dir.x, dir.y, dir.z),
+                            string.Format("Idle: {0}, Jumping: {1}, Leaning: {2}", mobility.IsIdle, mobility.IsJumping, mobility.IsLeaning),
+                            string.Format("Limping: {0}, Crouching: {1}, Eavesdropping: {2}", mobility.IsLimping, mobility.IsCrouching, mobility.IsEavesdropping),
+                            string.Format("Grounded: {0}, NearGrounded: {1}, Sneaking: {2}", mobility.IsGrounded, mobility.IsNearGrounded, mobility.IsSneaking),
+                            string.Format("Mantling: {0}, HoldingMantle: {1}, OnClimbable: {2}", mobility.IsMantling, mobility.IsHoldingMantle, mobility.OnClimbable),
+                            string.Format("IsRope: {0}, Sliding: {1}, OnMovingPlatform: {2}", mobility.IsRope, mobility.IsSliding, mobility.OnMovingPlatform),
+                            string.Format("FallHeight: {0}, LastFallHeight: {1}", mobility.FallHeight, mobility.LastFallHeight)
+                        };
+
+                        int y = 15;
+                        foreach (string s in stats)
+                        {
+                            y += 15;
+                            GUI.Label(new Rect(0, y, 500, 50), s);
+                        }
                     }
-                }
-                else
-                {
-                    // null case
-                    GUI.Label(new Rect(0, 15, 500, 50), "Cannot display further information; player is null.");
+                    else
+                    {
+                        // null case
+                        GUI.Label(new Rect(0, 30, 500, 50), "Cannot display further information; player is null.");
+                    }
                 }
             }
         }
